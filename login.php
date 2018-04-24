79c30cdf1ee77a6175690e1ab1617fd29d571013
Isaac Olawale <bodolawale@outlook.com>
2018-04-24 11:27:04 +0100

    basic css and js change

--- login.php
+++ login.php
@@ -35,17 +35,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css"/>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
     <title> Login </title>
 </head>
 <body>
-    
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-        <input type="email" name="email" placeholder="email">
-        <input type="password" name="password" placeholder="password">
-        <input type="submit" name="submit" value="Login">
-    </form>
+    <div class="text-center">
+        <h1 class="jumbotron">Heaven's Gate Hospital</h1>
+        <div>
+            <div class="card mx-auto" style="width:20rem;height:20rem">
+                <div class="card-body ">
+                    <div class="card-content ">
+                        <h3 class="card-heading ">Login</p>
+                    </div>
+                    <div>
+                        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                            <input type="email" name="email" placeholder="email" class="inputext">
+                            <input type="password" name="password" placeholder="password" class="inputext">
+                            <input type="submit" name="submit" value="Login" class="btn btn-primary">
+                        </form>
+                    </div>
+                </div>
 
-
-
+            </div>
+        </div>
+    </div>
 </body>
 </html>
