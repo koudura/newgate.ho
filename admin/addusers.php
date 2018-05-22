7b00a195f785eaf94a178b9343203e0f2f09885d
Isaac Olawale <bodolawale@outlook.com>
2018-05-22 18:26:26 +0100

    add logo

--- admin/addusers.php
+++ admin/addusers.php
@@ -31,7 +31,9 @@
 </head>
 <body>
     <div class="grid">
-        <div class = "logo">sss</div>
+        <div class = "logo">
+            <img class = "lago" src="../assets/images/newgate.svg" alt="logo here">
+        </div>
         <div class = "profile">adsdas</div>
         <div class = "navbar ">
                 <div class="damn">
