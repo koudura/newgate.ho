b1f0ce83ecc5d8e58cb8f73f13337bba6a95d5b1
mumetal <adulojuolatomiwa@gmail.com>
2018-05-24 05:38:19 +0100

    managepatient almost done

--- classes/prescription.php
+++ classes/prescription.php
@@ -1,18 +1,24 @@
 <?php
     class Prescription{
-        function __construct($ID, $diagnosisID, $name, $dose, $bill){
+        function __construct($ID, $diagnosisID, $name, $dosage, $bill){
             $this->ID = $ID;
             $this->diagnosisID = $diagnosisID;
             $this->name = $name;
-            $this->dose = $dose;
+            $this->dosage = $dosage;
             $this->bill = $bill;
         }
         
+        function saveToDB($conn){
+            $query = "INSERT INTO tbl_prescriptions(ID, diagnosisID, name, dosage, bill) VALUES(:ID, :diagnosisID, :name, :dosage, :bill)";
+            $stmt = $conn->prepare($query);
+            $stmt->execute(array('ID'=>null, 'diagnosisID'=>$this->diagnosisID, 'name'=>$this->name, 'dosage'=>$this->dosage, 'bill'=>$this->bill));
+        }
+
         static function getPrescriptionFromDB($conn, $ID){
             $query = "SELECT * FROM tbl_prescriptions WHERE ID=$ID";
             $stmt = $conn->query($query);
             $result = $stmt->fetchall(PDO::FETCH_ASSOC);
-            return new Prescription($ID, $result["diagnosisID"], $result["name"], $result["dose"], $result["bill"]);            
+            return new Prescription($ID, $result["diagnosisID"], $result["name"], $result["dosage"], $result["bill"]);            
         }
 
         static function getPrescriptionsFromDiagnosis($conn, $diagnosisID){
@@ -20,7 +26,7 @@
             $result = $stmt->fetchall(PDO::FETCH_ASSOC);
             $array = array();
             foreach ($result as $row) {
-                array_push($array, new Prescription($row[$ID], $diagnosisID, $result["name"], $result["dose"], $result["bill"]));
+                array_push($array, new Prescription($row[$ID], $diagnosisID, $result["name"], $result["dosage"], $result["bill"]));
             }
             return $array;            
         }
@@ -30,7 +36,7 @@
             $result = $stmt->fetchall(PDO::FETCH_ASSOC);
             $array = array();
             foreach ($result as $row) {
-                array_push($array, new Prescription($row[$ID], $diagnosisID, $result["name"], $result["dose"], $result["bill"]));
+                array_push($array, new Prescription($row[$ID], $diagnosisID, $result["name"], $result["dosage"], $result["bill"]));
             }
             return $array;            
         }
