64e0ffb659112237f2f06527ec4262a91e526321
Adedeji Ayomide <ayomideadedeji98@gmail.com>
2018-05-24 17:26:54 -0700

    add user questionnaire

--- pages/viewpatients.php
+++ pages/viewpatients.php
@@ -37,6 +37,7 @@ $patients = Patient::getAllPatients($conn);
     <nav>
         <a href="#" class="active dash_btn"><i class="fas fa-file-alt"></i>Patients Explorer</a>
         <a href="addpatients.php" class="dash_btn"><i class="fas fa-plus-circle"></i>Add Patient</a>
+        <a href="viewquestionnaires.php" class="dash_btn"><i class="fas fa-question"></i>Questionnaire</a>
         <a href="../pages/dashboard.php" class="dash_btn"><i class="fas fa-home"></i>Home</a>
         <a href="../logout.php" class="dash_btn"><i class="fas fa-sign-out-alt"></i>Logout</a>
     </nav>
--- pages/viewquestionnaires.php
+++ pages/viewquestionnaires.php
@@ -37,11 +37,9 @@ $quests = Questionnaire::getAll($conn);
 <section id="sideMenu">
     <img class="logo" src="../assets/images/newgate.svg" alt="logo here">
     <nav>
-        <a href="viewusers.php" class="dash_btn"><i class="fas fa-address-book"></i>User Explorer</a>
-        <a href="addusers.php" class="dash_btn"><i class="fas fa-plus-circle"></i>Add User</a>
+    <a href="viewpatients.php" class="dash_btn"><i class="fas fa-long-arrow-alt-left"></i>Back</a>
         <a href="#" class="active dash_btn"><i class="fas fa-question"></i>Questionnaire</a>
         <a href="../pages/dashboard.php" class="dash_btn"><i class="fas fa-home"></i>Home</a>
-        <a href="../logout.php" class="dash_btn"><i class="fas fa-sign-out-alt"></i>Logout</a>
     </nav>
 </section>
 <header>
